626ec45bf9e81d3dac3942791bf78f191b549a73
leno-mpm <s.pazmino16@gmail.com>
2025-05-28 18:13:18 -0500

    Apply the open-closed design principle in the CloudServicePaltform class

--- Taller Solid/src/CloudServicePlatform.java
+++ Taller Solid/src/CloudServicePlatform.java
@@ -1,17 +1,44 @@
+
+//SOLUCION DE LA VIOLACIÓN DEL PRINCIPIO OPEN-CLOSED
 public class CloudServicePlatform {
-    private int cloudServicePlatform;
+    private Cloud cloudProvider;
+  
+  	public CloudServicePlatform (Cloud cloudProvider){
+      	this.cloudProvider=cloudProvider;
+    }
     public void hostingTo (AppWeb app) {
-        if (cloudServicePlatform==1) {
-            System.out.println("Connect to AWS");
-            // Logic
-        }if (cloudServicePlatform==2) {
-            System.out.println("Connect to Microsoft Azure");
-            // Logic
-        }else{
-            System.out.println("Connect to Google Cloud");
-            // Logic
-        }
+      	cloudProvider.hostingTo(app); 
     }
     // More Methods
 }
 
+// Se agregó esta interfaz para aplicar el principio de abierto/cerrado (Open-Closed).
+// Ahora CloudServicePlatform no depende directamente de clases concretas como AWS o Azure,
+// sino que depende de una interfaz (Cloud), lo que permite extender el sistema
+// con nuevos servicios en la nube sin modificar el código existente.
+// Cada clase que represente un servicio de nube debe implementar esta interfaz.
+interface Cloud{
+  	void hostingTo(AppWeb app);
+}
+
+class SCAww implements Cloud{
+  	@Override 
+  	public void hostingTo(AppWeb app){
+      	System.out.println("Connect to AWS");
+    }
+}
+
+class SCMAzure implements Cloud{
+  	@Override 
+  	public void hostingTo(AppWeb app){
+      	System.out.println("Connect to Microsoft Azure");
+    }
+}
+
+
+class SCGoogleC implements Cloud{
+  	@Override 
+  	public void hostingTo(AppWeb app){
+      	System.out.println("Connect to Google Cloud");
+    }
+}
