5d19c8fa33a6fa892bb0251e8a416f0ae4d8f8e7
gabriel12305 <gpelaezavila@gmail.com>
2025-05-28 22:04:44 -0500

    final changes

--- Taller Solid/src/AppWeb.java
+++ Taller Solid/src/AppWeb.java
@@ -1,14 +1,11 @@
 public class AppWeb {
     LogIn logIn;
-    LogInAdmin logInAdmin;
-    MySQL mySQL;
-    public AppWeb (LogIn logIn, MySQL mySQL) {
+    DataBase dataBase;
+    
+    public AppWeb (LogIn logIn, DataBase dataBase) {
         // Logic
     }
-    public AppWeb (LogInAdmin logInAdmin, MySQL mySQL) {
-        // Logic
-    }
-    public void connectToDatabase (MySQL mySQL) {
+    public void connectToDatabase (DataBase dataBase) {
         // Logic
     }
 }
